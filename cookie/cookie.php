2da229a520459baab89cfb9e42084121d64434d6
arjun-sudo <arjungautam5431@gmail.com>
2021-01-16 10:00:08 +0545

    reformat

--- cookie/cookie.php
+++ cookie/cookie.php
@@ -2,24 +2,22 @@
 <html lang="en">
 
 <?php
-$cookie_name="user";
-$cookie_value="Arjun Gauttam";
- setcookie($cookie_name,$cookie_value,time()+(86400*30),"/");
+$cookie_name = "user";
+$cookie_value = "Arjun Gauttam";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 ?>
 
 <body>
-<?php
-if(!isset($_COOKIE[$cookie_name])){
-    echo "Cookie named'".$cookie_name."'is not set!";
-}
-else{
-    echo "Cookie '".$cookie_name."'is set !<br>";
-}
-    echo "Value is :".$_COOKIE[$cookie_name];
 
-?>
+    <?php
+    if (!isset($_COOKIE[$cookie_name])) {
+        echo "Cookie named'" . $cookie_name . "'is not set!";
+    } else {
+        echo "Cookie '" . $cookie_name . "'is set !<br>";
+    }
+    echo "Value is :" . $_COOKIE[$cookie_name];
 
+    ?>
 
 </body>
-
 </html>
