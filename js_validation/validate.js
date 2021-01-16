273568ad45475d2256ae5bfdc5cc9cbe14c9c443
arjun-sudo <arjungautam5431@gmail.com>
2021-01-16 17:28:10 +0545

    validation complete

--- js_validation/validate.js
+++ js_validation/validate.js
@@ -1,5 +1,5 @@
 function validateForm(){
-    var x=document.forms["myForm"]["name"].value;
+    var x=document.getElementById("fname").value;
     if(x.length<=5){
         alert("Name must be greater than 5");
         return false;
